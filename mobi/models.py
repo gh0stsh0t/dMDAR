e10044c6c014b942970e5f47a6b168932305a576
gh0stsh0t <rjperias0213@gmail.com>
2019-02-18 00:48:49 +0800

    Finished and Fixed models

--- mobi/models.py
+++ mobi/models.py
@@ -1,62 +1,10 @@
 from django.db import models
+from enum import Enum
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
-
-
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.EmailField()
-    password = models.CharField(max_length=20)
-    ADMIN, USER = True, False
-    usertypes = ((ADMIN, 'Admin'), (USER, 'User'))
-    usertype = models.BooleanField(choices=usertypes)
-    display_pic = models.ImageField()
-
-    def __str__(self):
-        return self.username
-
-
-class Movie(models.Model):
-    title = models.CharField()
-    isactive = models.BooleanField(default=True)
-    duration = models.IntegerField(validators=[MinValueValidator(0)])
-    synopsis = models.TextField()
-    G, PG, PG13, R, X = 'G', 'PG', 'PG-13', 'R', 'X'
-    restrictions = ((X, 'General Audience'), (PG, 'Parental Guidance Suggested'), (PG13, 'Parents Strongly Cautioned'),
-                    (R, 'Restricted '), (X, 'X-Rated'))
-    restriction = models.CharField(choices=restrictions)
-    release_date = models.DateField()
-    # special is used for trending and featured movies
-    TRENDING = 1
-    FEATURED = 2
-    NORMAL = 0
-    specials = ((TRENDING, 'Trending'), (FEATURED, 'Featured'), (NORMAL, 'Normal'))
-    special = models.IntegerField(choices=specials)
-    trailer = models.URLField()
-    director = models.CharField()
-    background = models.ImageField()
-    poster = models.ImageField()
-    time_posted = models.DateTimeField(auto_now_add=True)
-    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-
-class Review(models.Model):
-    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    comment = models.TextField()
-    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
-    time_posted = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return "Review by {} for {}".format(self.reviewer.username, self.movie.title)
-
-
-class Genre(models.Model):
+class GenreChoice(Enum):
     ACTION = 'Action'
     ADULT = 'Adult'
     ADVENTURE = 'Adventure'
@@ -86,8 +34,68 @@ class Genre(models.Model):
     THRILLER = 'Thriller'
     WAR = 'War'
     WESTERN = 'Western'
-    genres = ()
-    genre = models.CharField(choices=genres)
+
+
+class SiteChoice(Enum):
+    AMAZON = 'Amazon'
+    GOOGLE = 'Google Play'
+    ITUNES = 'iTunes'
+    NETFLIX = 'Netflix'
+
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    ADMIN, USER = True, False
+    usertypes = ((USER, 'User'), (ADMIN, 'Admin'))
+    usertype = models.BooleanField(choices=usertypes, default=usertypes[0][0])
+    display_pic = models.ImageField(null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    isactive = models.BooleanField(default=True)
+    duration = models.IntegerField(validators=[MinValueValidator(0)], null=True)
+    synopsis = models.TextField(null=True)
+    G, PG, PG13, R, X = 'G', 'PG', 'PG-13', 'R', 'X'
+    restrictions = ((X, 'General Audience'), (PG, 'Parental Guidance Suggested'), (PG13, 'Parents Strongly Cautioned'),
+                    (R, 'Restricted '), (X, 'X-Rated'))
+    restriction = models.CharField(choices=restrictions, max_length=5)
+    release_date = models.DateField(null=True)
+    # special is used for trending and featured movies
+    TRENDING = 1
+    FEATURED = 2
+    NORMAL = 0
+    specials = ((NORMAL, 'Normal'), (TRENDING, 'Trending'), (FEATURED, 'Featured'))
+    special = models.IntegerField(choices=specials, default=specials[0][0])
+    trailer = models.URLField(null=True)
+    director = models.CharField(max_length=75, null=True)
+    background = models.ImageField()
+    poster = models.ImageField()
+    time_posted = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    comment = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    time_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Review by {} for {}".format(self.reviewer.username, self.movie.title)
+
+
+class Genre(models.Model):
+    genre = models.CharField(choices=[(tag.value, tag.name) for tag in GenreChoice], max_length=15, primary_key=True)
     movies = models.ManyToManyField(Movie)
 
     def __str__(self):
@@ -97,19 +105,14 @@ class Genre(models.Model):
 class Actor(models.Model):
     firstname = models.CharField(max_length=35)
     lastname = models.CharField(max_length=35)
-    link = models.URLField()
-    movies = models.ManyToManyField(Movie)
+    link = models.URLField(null=True)
+    movies = models.ManyToManyField(Movie, blank=True, null=True)
 
     def __str__(self):
         return "{}, {}".format(self.lastname, self.firstname)
 
 
 class Watch(models.Model):
-    AMAZON = 'Amazon'
-    GOOGLE = 'Google Play'
-    ITUNES = 'iTunes'
-    NETFLIX = 'Netflix'
-    sites = ((AMAZON, 'Amazon'), (GOOGLE, 'Google Play'), (ITUNES, 'iTunes'), (NETFLIX, 'Netflix'))
-    site = models.CharField(choices=sites)
+    site = models.CharField(choices=[(tag.value, tag.name) for tag in SiteChoice], max_length=15)
     url = models.URLField()
-    movie = models.ForeignKey(Movie)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
