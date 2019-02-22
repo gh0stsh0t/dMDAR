e20ab540a23e1a4a11b7ac97f5e767b938ab56e3
AJ Pasigado <ajpasigado@outlook.com>
2019-02-22 18:40:45 +0800

    Added Templates; Added Static Files

---
+++ mobi/static/mobi/js/scripts.js
@@ -0,0 +1,251 @@
+window.onload = function() {
+    const r1 = new JSR(['#range-2-1', '#range-2-2'], {
+        sliders: 2,
+        min: 0,
+        max: 5,
+        step: 0.5,
+        values: [0, 5],
+        limit: {
+            show: false
+        },
+        labels: {
+            formatter: (value) => {
+                return value.toString();
+            }
+        }
+    }).setLimit('min', 0).setLimit('max', 5);
+    setTimeout(() => {
+        r1.refresh({
+            min: 0,
+            limit: {
+                min: 0,
+                max: 5
+            }
+        });
+    }, 2000);
+};
+
+
+
+$(function () {
+    $(window).on("scroll", function () {
+        if ($("#navbarSupportedContent").hasClass("show")){
+            $('.navbar-toggler').click();
+        }
+        if ($(window).scrollTop() > (window.innerHeight - 100)) {
+            $("#nav-bar").addClass("navigation-bar-color");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+            $("#nav-bar").removeClass("navigation-bar-color");
+        }
+    });
+    $(window).on("scroll", function () {
+        if ($("#navbarSupportedContent").hasClass("show")){
+            $('.navbar-toggler').click();
+        }
+        if ($(window).scrollTop() > 10) {
+            $("#nav-bar-normal").addClass("navigation-bar-color");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+            $("#nav-bar-normal").removeClass("navigation-bar-color");
+        }
+    });
+    $("#openSearch").on("click", function () {
+        if ($("#nav-bar-normal, #nav-bar").hasClass("nav-search-active") && $("#search").val() == "" && !$("#navbarSupportedContent").hasClass("show")){
+            $("#nav-bar-normal, #nav-bar").removeClass("nav-search-active");
+        } else {
+            $("#nav-bar-normal, #nav-bar").addClass("nav-search-active");
+        }
+    });
+    $("#openSearch").on("mouseover", function () {
+        if ($("#nav-bar-normal, #nav-bar").hasClass("nav-search-active") && $("#search").val() == "" && !$("#navbarSupportedContent").hasClass("show")){
+            $("#nav-bar-normal, #nav-bar").removeClass("nav-search-active");
+        } else {
+            $("#nav-bar-normal, #nav-bar").addClass("nav-search-active");
+        }
+    });
+    $("#search").on("mouseleave", function () {
+        if(!$("#search").is(":focus") && $("#search").val() == "" && !$("#navbarSupportedContent").hasClass("show")){
+            $("#nav-bar-normal, #nav-bar").removeClass("nav-search-active");
+        }
+    });
+    $("#search").on("focusout", function () {
+        if($("#search").val() == "" && !$("#navbarSupportedContent").hasClass("show")){
+            $("#nav-bar-normal, #nav-bar").removeClass("nav-search-active");
+        }
+    });
+    $("#nav-toggle").on("click", function () {
+        if (!$("#navbarSupportedContent").hasClass("show")){
+            $("#nav-bar-normal, #nav-bar").addClass("nav-search-active navigation-bar-color navigation-bar-show ");
+            $("#openSearch").hide();
+        } else {
+            $("#nav-bar-normal, #nav-bar").removeClass("nav-search-active navigation-bar-color navigation-bar-show ");
+            $("#openSearch").show();
+        }
+    });
+
+    var starRating = 0;
+    $("#write-review-star1").on("mouseenter", function () {
+        $("#write-review-star1").addClass("checked");
+        $("#write-review-star2").removeClass("checked");
+        $("#write-review-star3").removeClass("checked");
+        $("#write-review-star4").removeClass("checked");
+        $("#write-review-star5").removeClass("checked");
+    });
+    $("#write-review-star1").on("click", function () {
+        if (!$("#write-review-star1").hasClass("selected")) $("#write-review-star1").addClass("selected");
+        if ($("#write-review-star2").hasClass("selected")) $("#write-review-star2").removeClass("selected");
+        if ($("#write-review-star3").hasClass("selected")) $("#write-review-star3").removeClass("selected");
+        if ($("#write-review-star4").hasClass("selected")) $("#write-review-star4").removeClass("selected");
+        if ($("#write-review-star5").hasClass("selected")) $("#write-review-star5").removeClass("selected");
+        starRating = 1;
+    });
+    $("#write-review-star2").on("mouseenter", function () {
+        $("#write-review-star1").addClass("checked");
+        $("#write-review-star2").addClass("checked");
+        $("#write-review-star3").removeClass("checked");
+        $("#write-review-star4").removeClass("checked");
+        $("#write-review-star5").removeClass("checked");
+    });
+    $("#write-review-star2").on("click", function () {
+        if (!$("#write-review-star1").hasClass("selected")) $("#write-review-star1").addClass("selected");
+        if (!$("#write-review-star2").hasClass("selected")) $("#write-review-star2").addClass("selected");
+        if ($("#write-review-star3").hasClass("selected")) $("#write-review-star3").removeClass("selected");
+        if ($("#write-review-star4").hasClass("selected")) $("#write-review-star4").removeClass("selected");
+        if ($("#write-review-star5").hasClass("selected")) $("#write-review-star5").removeClass("selected");
+        starRating = 2;
+    });
+    $("#write-review-star3").on("mouseenter", function () {
+        $("#write-review-star1").addClass("checked");
+        $("#write-review-star2").addClass("checked");
+        $("#write-review-star3").addClass("checked");
+        $("#write-review-star4").removeClass("checked");
+        $("#write-review-star5").removeClass("checked");
+    });
+    $("#write-review-star3").on("click", function () {
+        if (!$("#write-review-star1").hasClass("selected")) $("#write-review-star1").addClass("selected");
+        if (!$("#write-review-star2").hasClass("selected")) $("#write-review-star2").addClass("selected");
+        if (!$("#write-review-star3").hasClass("selected")) $("#write-review-star3").addClass("selected");
+        if ($("#write-review-star4").hasClass("selected")) $("#write-review-star4").removeClass("selected");
+        if ($("#write-review-star5").hasClass("selected")) $("#write-review-star5").removeClass("selected");
+        starRating = 3;
+    });
+    $("#write-review-star4").on("mouseenter", function () {
+        $("#write-review-star1").addClass("checked");
+        $("#write-review-star2").addClass("checked");
+        $("#write-review-star3").addClass("checked");
+        $("#write-review-star4").addClass("checked");
+        $("#write-review-star5").removeClass("checked");
+    });
+    $("#write-review-star4").on("click", function () {
+        if (!$("#write-review-star1").hasClass("selected")) $("#write-review-star1").addClass("selected");
+        if (!$("#write-review-star2").hasClass("selected")) $("#write-review-star2").addClass("selected");
+        if (!$("#write-review-star3").hasClass("selected")) $("#write-review-star3").addClass("selected");
+        if (!$("#write-review-star4").hasClass("selected")) $("#write-review-star4").addClass("selected");
+        if ($("#write-review-star5").hasClass("selected")) $("#write-review-star5").removeClass("selected");
+        starRating = 4;
+    });
+    $("#write-review-star5").on("mouseenter", function () {
+        $("#write-review-star1").addClass("checked");
+        $("#write-review-star2").addClass("checked");
+        $("#write-review-star3").addClass("checked");
+        $("#write-review-star4").addClass("checked");
+        $("#write-review-star5").addClass("checked");
+    });
+    $("#write-review-star5").on("click", function () {
+        if (!$("#write-review-star1").hasClass("selected")) $("#write-review-star1").addClass("selected");
+        if (!$("#write-review-star2").hasClass("selected")) $("#write-review-star2").addClass("selected");
+        if (!$("#write-review-star3").hasClass("selected")) $("#write-review-star3").addClass("selected");
+        if (!$("#write-review-star4").hasClass("selected")) $("#write-review-star4").addClass("selected");
+        if (!$("#write-review-star5").hasClass("selected")) $("#write-review-star5").addClass("selected");
+        starRating = 5;
+    });
+    $("#write-review-star-group").on("mouseleave", function () {
+        if (!$("#write-review-star1").hasClass("selected")) $("#write-review-star1").removeClass("checked");
+        else $("#write-review-star1").addClass("checked");
+    
+        if (!$("#write-review-star2").hasClass("selected")) $("#write-review-star2").removeClass("checked");
+        else $("#write-review-star2").addClass("checked");
+
+        if (!$("#write-review-star3").hasClass("selected")) $("#write-review-star3").removeClass("checked");
+        else $("#write-review-star3").addClass("checked");
+
+        if (!$("#write-review-star4").hasClass("selected")) $("#write-review-star4").removeClass("checked");
+        else $("#write-review-star4").addClass("checked");
+
+        if (!$("#write-review-star5").hasClass("selected")) $("#write-review-star5").removeClass("checked");
+        else $("#write-review-star5").addClass("checked");
+    });
+    $('#user-reviews-table tr').hover(function() {
+        $(this).addClass('user-reviews-actions-show');
+    }, function() {
+        $(this).removeClass('user-reviews-actions-show');
+    });
+
+});
+
+
+(function() {
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.getElementById('scrollId').scrollLeft -= (delta*40); // Multiplied by 40
+        e.preventDefault();
+    }
+    if (document.getElementById('scrollId').addEventListener) {
+        // IE9, Chrome, Safari, Opera
+        document.getElementById('scrollId').addEventListener("mousewheel", scrollHorizontally, false);
+        // Firefox
+        document.getElementById('scrollId').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+    } else {
+        // IE 6/7/8
+        document.getElementById('scrollId').attachEvent("onmousewheel", scrollHorizontally);
+    }
+})();
+
+(function() {
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.getElementById('scrollId2').scrollLeft -= (delta*40); // Multiplied by 40
+        e.preventDefault();
+    }
+    if (document.getElementById('scrollId2').addEventListener) {
+        // IE9, Chrome, Safari, Opera
+        document.getElementById('scrollId2').addEventListener("mousewheel", scrollHorizontally, false);
+        // Firefox
+        document.getElementById('scrollId2').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+    } else {
+        // IE 6/7/8
+        document.getElementById('scrollId2').attachEvent("onmousewheel", scrollHorizontally);
+    }
+})();
+
+(function() {
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.getElementById('scrollId3').scrollLeft -= (delta*40); // Multiplied by 40
+        e.preventDefault();
+    }
+    if (document.getElementById('scrollId3').addEventListener) {
+        // IE9, Chrome, Safari, Opera
+        document.getElementById('scrollId3').addEventListener("mousewheel", scrollHorizontally, false);
+        // Firefox
+        document.getElementById('scrollId3').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+    } else {
+        // IE 6/7/8
+        document.getElementById('scrollId3').attachEvent("onmousewheel", scrollHorizontally);
+    }
+})();
+
+function showEditReview(id){
+    $("#edit-review-modal").modal({
+        backdrop: 'static',
+        keyboard: false
+    })
+}
+
+function archiveReview(id) {
+    alert(id);
+}
