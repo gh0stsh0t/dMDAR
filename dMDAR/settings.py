622cacd127d05fb752264c385452819a17c4c6c4
gh0stsh0t <rjperias0213@gmail.com>
2019-03-04 23:34:25 +0800

    Finished the following:
    CATALOG
    Ratings Filter ✔
    Genre (list) ✔
    
    DETAILS
    Watch Now
    Image Artist ✔
    Role ✔
    Summary
    - Overall ✔
    - Count of Reviews ✔
    - 5, 4, 3, 2, 1 Overall ✔
    
    USER PAGE
    Reviews
    - Ratings ✔
    - Status ✔
    Movies
    - Status ✔
    - Same sa catalog ✔
    
    WEBSITE GLOBAL
    - Social Media Links ✔
    -- Fb, Twitter, Insta, Snap, Youtub ✔

--- dMDAR/settings.py
+++ dMDAR/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mobi.apps.MobiConfig',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'dMDAR.processor.soc_media'
             ],
         },
     },
